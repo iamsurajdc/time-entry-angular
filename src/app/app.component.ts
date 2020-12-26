f39d36763448c007230007b3686fbe3bd7668f35
Suraj Chandgude <surajchandgude0304@gmail.com>
2020-12-26 18:31:16 +0530

    📦 NEW: time entry initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { differenceInMinutes, differenceInHours } from 'date-fns';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'time-entry-ng';
+  title = 'Time Entry';
+  timeIn: Date = new Date();
+  timeOut: Date = new Date();
+  isMeridian: boolean = false;
+  minTime: Date = new Date();
+  maxTime: Date = new Date();
+
+  dataSource: any = [];
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSave() {}
+
+  Addentry() {
+    console.log(`in and out`, this.timeIn, this.timeOut);
+    let diffHours = differenceInHours(this.timeIn, this.timeOut);
+    let diffMinutes = differenceInMinutes(this.timeIn, this.timeOut);
+
+    let duration = `${Math.abs(diffHours)}: ${
+      Math.abs(diffMinutes) % 2 == 0 ? '00' : Math.abs(diffMinutes)
+    }`;
+
+    console.log(`duration`, duration);
+
+    let el = {
+      in: this.timeIn,
+      out: this.timeOut,
+      duration: duration,
+    };
+    this.dataSource.push(el);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimepickerModule } from 'ngx-bootstrap/timepicker';
+import { DxSelectBoxModule, DxDataGridModule } from 'devextreme-angular';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    AppRoutingModule,
+    TimepickerModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+    DxSelectBoxModule,
+    DxDataGridModule,
   ],
+  exports: [FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  schemas: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
